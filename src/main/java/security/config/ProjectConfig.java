ed3c4f63fa8de14d6abfb37b576ea16eeb5e80d8
Pavel Vydysh <vydysh.pavel@gmail.com>
2025-09-07 21:16:16 +0300

    core

---
+++ src/main/java/security/config/ProjectConfig.java
@@ -0,0 +1,51 @@
+package security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+public class ProjectConfig {
+
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//        UserDetails details = User.withUsername("john")
+//                .password("123456")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(details);
+//    }
+//
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        UserDetails details = User.withUsername("john")
+                .password("123456")
+                .authorities("read")
+                .build();
+
+        UserDetailsService service = new InMemoryUserDetailsManager(details);
+
+        httpSecurity.userDetailsService(service);
+        return httpSecurity.build();
+    }
+
+}
