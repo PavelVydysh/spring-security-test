88982d644392569a61fda02df84fe8df3aecf119
Pavel Vydysh <vydysh.pavel@gmail.com>
2025-09-16 21:07:54 +0300

    passwords

---
+++ password-work/src/main/java/security/encoder/PlainTextPasswordEncoder.java
@@ -0,0 +1,20 @@
+package security.encoder;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlainTextPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return rawPassword.toString();
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return rawPassword.toString().equals(encodedPassword);
+    }
+
+}
